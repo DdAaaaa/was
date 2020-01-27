ff6a6e05eb670af59ad6f0936855cce6216f36d7
parkchansoo <chansoo1563@gmail.com>
2020-01-27 16:10:30 +0900

    delete sentence model

--- was/models.py
+++ was/models.py
@@ -1,22 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class QuaterBlock(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  level = models.IntegerField(default=0)
-  created_date = models.DateTimeField()
-  modified_date = models.DateTimeField()
-  def __str__(self):
-      return self.level
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    level = models.IntegerField(default=0)
+    created_date = models.DateTimeField()
+    modified_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.level
+
 
 class Goal(models.Model):
-  user = models.ForeignKey(User, on_delete=models.CASCADE)
-  goal = models.IntegerField()
-  def __str__(self):
-      return self.goal
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goal = models.IntegerField()
 
-class Sentences(models.Model):
-  percentage = models.IntegerField()
-  sentences = models.TextField()
-  def __str__(self):
-      return self.sentences
+    def __str__(self):
+        return self.goal
+
